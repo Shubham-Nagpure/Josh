73c0dd73723805828b9a09d1e771de60403c8f79
shubham_ubuntu <nagpure.sr7@gmail.com>
2019-12-16 20:41:09 +0530

    update in prime file

--- prime.rb
+++ prime.rb
@@ -2,11 +2,13 @@ print"Enter number to check it is prime or not"
 number=gets.to_i
 
 itr=2
-while(number%itr!=0)
-	itr+=1
+for a in 2..number/2
+	if(number%itr!=0)
+		break
+	end
 end
-if number==itr
+if number/2<=itr
 	print"given number is prime"
 else
-	print"given number is npot prime"
+	print"given number is not prime"
 end
