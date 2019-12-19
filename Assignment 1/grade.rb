eab5745002706f913d8dc1c0ae51ad1162d08258
shubham_ubuntu <nagpure.sr7@gmail.com>
2019-12-19 16:01:46 +0530

    Updated basic grade program

--- Assignment 1/grade.rb
+++ Assignment 1/grade.rb
@@ -1,15 +1,12 @@
 print"Enter Grade:"
-grade=gets.to_i
+grade=gets.chomp.to_i
 
-if grade<=0
-  p "enter valid grade"
-elsif grade>0 and grade<6
-  p "elementry"
-elsif grade>5 and grade<9
-  p "middle school"
-elsif grade>8 and grade<=12
-  p "high school"
-else
-  print"college"
-end
+level = case grade
+        when 1..5 then "elementry"
+        when 6..8 then "middle school"
+        when 9..12 then "high school"
+        else "college"
+        end
+
+puts level
 
